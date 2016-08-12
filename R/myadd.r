c8c3a7d40d89dec57cac7cb0cc4ad6f66e1d3888
Tri Kurniawan Wijaya <trikurniawan.wijaya@trivago.com>
2016-08-12 15:50:13 +0200

    Test function: Add

---
+++ R/myadd.r
@@ -0,0 +1,12 @@
+#' An Integer Addition Function
+#'
+#' This function allows you to add two numbers.
+#' @param a the first number
+#' @param b the second number, default to 0
+#' @keywords add myadd
+#' @export
+#' @examples
+#' myadd(2,3)
+myadd <- function(a, b=0) {
+  return(a+b)
+}
